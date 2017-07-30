ccc40b2671a0efd21ea88d26a596137ca63ea8d0
Taylor Adamson <tayloradamson@outlook.com>
2017-07-30 02:45:21 -0600

    Changed output directory structure to consist of a single folder of images, and a JSON file with all entry data. Scores are now also maintained in a JSON file. Scraper has been refactored to accommodate these changes

--- scrape.py
+++ scrape.py
@@ -18,19 +18,19 @@ streak_interval = 1
 scale = 1
 flags = {
 	"export_scores": False, # Write raw score data
-	"export_archive": False, # Write formatted scoring list with archive-related data
-	"export_json": True
+	"export_archive": True, # Write formatted scoring list
+	"export_scraped": False # Write scraped data including images and json
 }
 now = datetime.now()
 date = str(now.month) + "." + str(now.day) + "." + str(now.year)
 parent = "2017/" + date
 fake_time = 1483976641887
-devs = []
+devs = [] # Container for all devs participating this week
 ignore = [] # Set of game names to ignore for this pass if people submit entries twice for some reason
 score_dict = {}
 
-def clamp(arg, value):
-	if arg < value:
+def clamp(arg, value, flip = 1):
+	if (arg * flip) < (value * flip):
 		return value
 	else:
 		return arg
@@ -121,10 +121,9 @@ def scoring(dev):
 		score = score + int(score_interval * scale * mult)
 		if not returning:
 			mult = mult + (mult_interval * scale)
-		if mult > max_mult:
-			mult = max_mult
+		clamp(mult, max_mult, -1)
 		streak = streak + (streak_interval * scale)
-	# Store current score data
+	# Store new score data
 	dict["score"] = str(score)
 	dict["mult"] = str(mult)
 	dict["streak"] = str(streak)
@@ -136,19 +135,16 @@ def scoring(dev):
 	dev["scoring"] = scoring
 
 # Load scoring data
-scores = open("score_data.txt", "r").readlines()
-for line in scores:
-	string = line.split("#")
-	string[5] = string[5].replace("\n", "")
-	reset = int(string[4]) - scale
-	reset = clamp(reset, -1)
-	inactive = int(string[5]) + scale
-	score_dict[string[0]] = {"score": string[1], "mult": string[2], "streak": string[3], "reset": str(reset), "inactive": str(inactive)}
+with open("scores.json") as file:
+	score_dict = json.load(file)
+	for game, dict in score_dict.items():
+		dict["reset"] = str(clamp(int(dict["reset"]) - 1, -1))
+		dict["inactive"] = str(int(dict["inactive"]) + scale)
 
 # Get threads to scrape, then scrape them
 threads = open("threads.txt", "r").readlines()
 for thread in threads:
-	url = "https://a.4cdn.org/vg/thread/" + str.strip(thread, "\n") + ".json"
+	url = "https://a.4cdn.org/vg/thread/" + thread.rstrip() + ".json"
 	# Fetch thread URL data
 	thread = get(url)
 	if thread.status_code != success:
@@ -166,15 +162,14 @@ for thread in threads:
 				continue
 			devs.append(dev)
 
-if flags.get("export_json"):
-	data = {}
-	if (not os.path.isdir(parent)):
+if flags.get("export_scraped"):
+	if not os.path.isdir(parent):
 		os.makedirs(parent)
 		os.makedirs(parent + "/images")
+	data = {}
 	for dev in devs:
 		with open(parent + "/images/" + dev["tim"] + dev["ext"], "wb") as output:
 			output.write(dev["image"].content)
-		# Prepare data for JSON dump
 		temp = {}
 		temp["game"] = dev["game"]
 		temp["name"] = dev["name"]
@@ -182,30 +177,18 @@ if flags.get("export_json"):
 		temp["web"] = dev["web"]
 		temp["scoring"] = dev["scoring"]
 		temp["ext"] = dev["ext"]
-		list = []
-		for item in dev["progress"]:
-			list.append(item)
-		temp["progress"] = list
-		data[dev["tim"]] = temp\
-	# Write JSON
+		temp["progress"] = dev["progress"]
+		data[dev["tim"]] = temp
 	open(parent + "/data.json", "w").close()
 	with open(parent + "/data.json", "w") as output:
 		json.dump(data, output, indent = 4)
 
-# Scores
 if flags.get("export_scores"):
-	open("score_data.txt", "w").close()
-	with open("score_data.txt", "w") as output:
-		for game, dev_dict in score_dict.items():
-			output.write(game + "#" +
-				str(dev_dict.get("score"))+ "#" +
-				str(dev_dict.get("mult")) + "#" +
-				str(dev_dict.get("streak")) + "#" +
-				str(dev_dict.get("reset")) + "#" +
-				str(dev_dict.get("inactive")) +
-				"\n")
-	# Scores backup
-	shutil.copy2("score_data.txt", "archives/scores/score_data_" + date + ".txt")
+	open("scores.json", "w").close()
+	with open("scores.json", "w") as output:
+		json.dump(score_dict, output, indent = 4)
+	# Store copy in week folder and root folder
+	shutil.copy2("scores.json", parent + "/scores.json")
 
 def tier_iter(range_token, set, last_tier):
 	output.write(range_token + "\n")
@@ -213,14 +196,14 @@ def tier_iter(range_token, set, last_tier):
 	for game in set:
 		sort[game] = int(score_dict[game].get("score"))
 	sort = sorted(sort, key = sort.get, reverse = True)
+	newl = ""
 	for game in sort:
-		output.write(score_dict[game].get("score") + "  --  " + game)
+		output.write(newl + score_dict[game].get("score") + "  --  " + game)
+		newl = "\n"
 		if (int(score_dict[game].get("reset")) > 0) and (float(score_dict[game].get("mult")) > 1):
-			output.write(" (x" + score_dict[game].get("mult") + ", " + score_dict[game].get("streak") + " streak)\n")
-		else:
-			output.write("\n")
+			output.write(" (x" + score_dict[game].get("mult") + ", " + score_dict[game].get("streak") + " streak)")
 	if not last_tier:
-		output.write("\n")
+		output.write("\n\n")
 
 def load_tiers(check_inactive):
 	tier_1 = [] # 1 to 199
@@ -245,14 +228,11 @@ def load_tiers(check_inactive):
 			append_check(game)
 	return {"one": tier_1, "two": tier_2, "three": tier_3, "four": tier_4}
 
-# Scores archive export
 if flags.get("export_archive"):
-	open("score_archive.txt","w").close()
-	with open("score_archive.txt", "w") as output:
+	open("scores.txt", "w").close()
+	with open("scores.txt", "w") as output:
 		tiers = load_tiers(False)
 		tier_iter("[ 1000+ ]", tiers.get("four"), False)
 		tier_iter("[ 500 - 999 ]", tiers.get("three"), False)
 		tier_iter("[ 200 - 499 ]", tiers.get("two"), False)
 		tier_iter("[ 1 - 199 ]", tiers.get("one"), True)
-	# Scores archive backup
-	shutil.copy2("score_archive.txt", "archives/scores/score_archive_" + date + ".txt")
